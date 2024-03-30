ac3d99ec31bf46e57ac521500cc8a1aaa1213676
Bagus Cahyono <baguscy8@gmail.com>
2024-03-30 09:51:55 +0700

    feat: slicing login & register

--- resources/js/Components/TextInput.jsx
+++ resources/js/Components/TextInput.jsx
@@ -1,6 +1,24 @@
-import { forwardRef, useEffect, useRef } from 'react';
+import { forwardRef, useEffect, useRef } from "react";
+import PropTypes from "prop-types";
 
-export default forwardRef(function TextInput({ type = 'text', className = '', isFocused = false, ...props }, ref) {
+export default forwardRef(function TextInput(
+    {
+        type = "text",
+        name,
+        value,
+        defaultValue,
+        className = "",
+        variant = "primary",
+        autoComplete,
+        required,
+        isFocused,
+        handleChange,
+        placeholder,
+        isError,
+        ...props
+    },
+    ref
+) {
     const input = ref ? ref : useRef();
 
     useEffect(() => {
@@ -13,11 +31,29 @@ export default forwardRef(function TextInput({ type = 'text', className = '', is
         <input
             {...props}
             type={type}
-            className={
-                'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm ' +
-                className
-            }
+            className={`rounded-2xl bg-form-bg py-[13px] px-7 w-full ${
+                isError && "input-error"
+            } input-${variant} ${className}`}
+            name={name}
+            value={value}
+            autoComplete={autoComplete}
             ref={input}
+            placeholder={placeholder}
         />
     );
+
+    TextInput.propTypes = {
+        type: PropTypes.oneOf(["text", "email", "password", "number", "file"]),
+        name: PropTypes.string,
+        value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        defaultValue: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+        className: PropTypes.string,
+        variant: PropTypes.oneOf(["primary", "error", "primary-outline"]),
+        autoComplete: PropTypes.string,
+        required: PropTypes.bool,
+        isFocused: PropTypes.bool,
+        handleChange: PropTypes.func,
+        placeholder: PropTypes.string,
+        isError: PropTypes.bool,
+    };
 });
